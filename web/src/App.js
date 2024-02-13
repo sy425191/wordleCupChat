1b2e6781848851d715e0b2138ac5855d85c9e45d
saurabh yadav <sy425191@gmail.com>
2024-02-13 16:40:39 +0530

    Initial Commit

---
+++ web/src/App.js
@@ -0,0 +1,11 @@
+import LandingPage from "./pages/Landing.js";
+
+function App() {
+  return (
+    <div className="App text-5xl">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/pages/Landing.js
@@ -0,0 +1,9 @@
+const LandingPage = () => {
+    return (
+        <div className="w-full h-screen flex justify-center items-center bg-slate-300">
+            I'm the landing page
+        </div>
+    );
+}
+
+export default LandingPage;
