aa83d2c5376d7c572dacc8258caaba876b8111b7
saurabh yadav <sy425191@gmail.com>
2024-02-13 20:50:53 +0530

    Basic implementation of wordle

--- web/src/components/chatRoom/chatSidebar.js
+++ web/src/components/chatRoom/chatSidebar.js
@@ -1,44 +1,18 @@
+import { useState } from "react";
 import WordleInputBox from "./wordleinput";
 
 const WordlePlayerBarComponent = () => {
+  const [activeWordleInput, setActiveWordleInput] = useState(0);
+
+  const wordleInputBoxes = Array(30)
+    .fill(0)
+    .map((_, i) => <WordleInputBox key={i} />);
   return (
     <div className="hidden md:flex flex-col justify-evenly items-center w-1/3 h-full rounded-xl p-3 bg-slate-400">
       <div className="min-w-64 w-1/2 h-72 p-1 border-4 border-slate-700 grid grid-cols-5 rounded-lg bg-slate-100">
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
-        <WordleInputBox />
+        {wordleInputBoxes.map((box, index) => (
+          <WordleInputBox key={index} index={index} activeWordleInput={activeWordleInput} setActiveWordleInput={setActiveWordleInput} />
+        ))}
       </div>
 
       <div className="text-xl uppercase text-slate-800 w-2/3 text-center font-semibold">
--- web/src/components/chatRoom/wordleinput.js
+++ web/src/components/chatRoom/wordleinput.js
@@ -1,9 +1,36 @@
-const WordleInputBox = () => {
+import { useEffect, useRef, useState } from "react";
+
+const WordleInputBox = ({ index, activeWordleInput, setActiveWordleInput }) => {
+  const [input, setInput] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (activeWordleInput === index) {
+      inputRef.current.focus();
+    }
+  }, [activeWordleInput]);
+
   return (
     <input
       type="text"
       className="w-full h-full border-2 outline-none border-slate-500 rounded bg-slate-100 text-xl font-bold text-center uppercase"
       maxLength={1}
+      disabled={activeWordleInput !== index ? true : false}
+      // onChange={setActiveWordleInput((prev) => prev+1)}
+      value={input}
+      ref={inputRef}
+      onChange={(e) => {
+        setInput(e.target.value);
+        if (activeWordleInput < 29) {
+          setActiveWordleInput((prev) => prev + 1);
+        }
+      }}
+      onKeyDown={(e) => {
+        if (e.key === "Backspace" && activeWordleInput > 0) {
+          setInput("");
+          setActiveWordleInput((prev) => prev - 1);
+        }
+      }}
     />
   );
 };
